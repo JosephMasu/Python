d57458458dbe85690476fa25c192bac4b32f29de
josephMasu <josephmuhindo089@gmail.com>
2025-03-17 23:50:54 +0300

    Typecasting

---
+++ typecasting/one.py
@@ -0,0 +1,26 @@
+# typecasting: is a processe of coverting one datatype into another
+
+name = 'Masu Joe'
+age = 26
+gpa = 3.2
+is_student = True
+
+gpa = int(gpa)
+# age = float(age)
+age = str(age)
+age += '2'
+
+if name =={''}:
+    print('name is empty')
+else:
+    print('name is not empty')
+
+
+name  = bool(name)
+print(name)
+
+print(gpa)
+# print(age)
+print(age)
+
+
