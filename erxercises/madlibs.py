2ee245b6e5225224005e4a9067169312c9b3485c
josephMasu <josephmuhindo089@gmail.com>
2025-03-18 01:23:57 +0300

    Madlibs Game

---
+++ erxercises/madlibs.py
@@ -0,0 +1,14 @@
+# Madlibs Game
+# word ga,e where you create story 
+# by filling in blanks with random words
+
+adjective1 = input("Enter an adjective: ")
+name1 = input("Enter a name: ")
+verb1 = input("Enter a verb: ")
+noun1 = input("Enter a noun1: ")
+noun2 = input("Enter a noun2: ")
+
+print("Welcome to Madlibs!")
+print(f"I went to see my {adjective1} GF at a restaurant")
+print(f"her name is {name1}")
+print(f"she {verb1} {noun1}, she ended up not finishing the {noun2}")
