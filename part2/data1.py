5252352d108205953317c9f4d8eaaa6cb40bbca5
josephMasu <josephmuhindo089@gmail.com>
2025-03-17 23:03:42 +0300

    all data types, int, string, float, booleans

---
+++ part2/data1.py
@@ -0,0 +1,36 @@
+#Strings
+
+first_name ='Masu'
+food ='ugali'
+email='masu123@fakegmail.com'
+
+print(f'Hello {first_name}')
+print(f'your fave food is :{food}')
+print(f'your email is :{email}')
+
+# integers
+
+age = 26
+quantity = 5
+
+print(f'you are buying {quantity}i tems ')
+print(f'you are {age} years old')
+
+#floats
+
+price = 10.99
+GPA = 3.2
+
+print(f'your GPA is {GPA}')
+print(f'the price is ${price}')
+
+
+#BOOLEANS
+
+is_student = False
+is_admin = True
+has_email = True
+
+print(f'you are a student : {is_student}')
+print(f'you are an admin : {is_admin}')
+print(f'you have an email : {has_email}')
