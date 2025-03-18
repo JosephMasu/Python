ab30094e455e52f9f66fba215a9c8c93abe64afa
josephMasu <josephmuhindo089@gmail.com>
2025-03-18 11:50:12 +0300

    math methods

---
+++ arthmetic and math/two.py
@@ -0,0 +1,15 @@
+#round() covert a variable to the nearest non decimal number(int)
+
+x = 3.14
+y = 4
+you = -4
+z =5
+
+# result = round(x)
+# result = abs(you)
+# result = pow(y,z)
+# result = max(x,y,z)
+result = min(x,y,z)
+
+print(result)
+
