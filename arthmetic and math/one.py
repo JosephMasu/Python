06aaa29d5ba3ddc6cf9a44248719b8c7ad189006
josephMasu <josephmuhindo089@gmail.com>
2025-03-18 11:42:32 +0300

    arithmetic

---
+++ arthmetic and math/one.py
@@ -0,0 +1,19 @@
+# arthmetic 
+
+friends = 13
+# friends = friends + 1
+# friends += 1
+# friends = friends - 1
+# friends -= 1
+# friends = friends / 1
+# friends /= 1
+# friends = friends * 1
+# friends *= 1
+# friends = friends ** 1
+# friends **= 1
+# friends = friends % 2
+friends %= 2
+
+
+
+print(friends)
