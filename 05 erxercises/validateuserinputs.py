7819ba101c094b30488485cd7eb9bac03d6adaab
josephMasu <josephmuhindo089@gmail.com>
2025-03-19 16:25:26 +0300

    correcting the logic

--- 05 erxercises/validateuserinputs.py
+++ 05 erxercises/validateuserinputs.py
@@ -1,6 +1,10 @@
 username = input('Enter your name: ')
 
-if len(username) > 12 and not username.find(' ') == -1 and not username.isdigit():
-    print('Your user name can not be more than 12 chracters and can not contain spaces or digits')
-else:
-    print(f'Your name is {username}')
+if len(username) > 12:
+    print('Your user name can not be more than 12 chracters.')
+elif not username.find(' ') == -1:
+    print('Your username can not contain spaces')
+elif not username.isdigit():
+    print('Your username can not usernam')
+else:    
+    print(f'Hello {username}')
