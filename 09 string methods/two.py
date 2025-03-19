24392b778dc811e104243a02d81ef0e80beb0499
josephMasu <josephmuhindo089@gmail.com>
2025-03-19 17:37:26 +0300

    string indexing

---
+++ 09 string methods/two.py
@@ -0,0 +1,8 @@
+# accessing eleement odf a string using []
+
+credit_number = "1234-5678-9012-3456"
+print(credit_number[0:4])
+print(credit_number[5:9])
+print(credit_number[10:14])
+print(credit_number[15:19])
+
