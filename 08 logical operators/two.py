41e4e876420585ac40025a8d14b42a84f89c00cd
josephMasu <josephmuhindo089@gmail.com>
2025-03-19 15:37:39 +0300

    conditional expersiion

--- 08 logical operators/two.py
+++ 08 logical operators/two.py
@@ -1,6 +1,7 @@
 gpa = 2.0
 attendance = True  
-
+pass_status = 'Yes' if gpa >= 2.5 else 'No'
+print(pass_status)
 if gpa >= 4 or (2.5 <= gpa < 4 and attendance):
     print("Congratulations, you have passed!")
 elif gpa < 2.5 and not attendance:
