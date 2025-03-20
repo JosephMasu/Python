f329ae75696a895276efbfeb63c2744a322595b3
josephMasu <josephmuhindo089@gmail.com>
2025-03-20 14:17:41 +0300

    collection: tuple

---
+++ 12 collection/tuple.py
@@ -0,0 +1,11 @@
+#  atuple(): ordered and unchangeable. duplicate, it's faster.
+
+
+languages =('c', 'java', 'python', 'javascript')
+
+# print(dir(languages))
+# print(help(languages))
+
+print(languages.index('python'))
+print(languages.count('python'))
+print(languages)
