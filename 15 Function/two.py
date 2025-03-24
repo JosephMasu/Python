6c4f3c19ba4f5de040852facc3b4363d587bb416
josephMasu <josephmuhindo089@gmail.com>
2025-03-24 22:55:30 +0300

    return type fucntion

---
+++ 15 Function/two.py
@@ -0,0 +1,28 @@
+def add(x,y):
+    z = x + y
+    return z
+
+def substract(x,y):
+    z = x - y
+    return z
+
+def multiply(x,y):
+    z = x * y
+    return z
+
+def divid(x,y):
+    z = x / y
+    return z
+    
+def modulas(x,y):
+    z = x % y
+    return z
+    
+
+print(add(2,3))
+print(substract(2,3))
+print(multiply(2,3))
+print(divid(2,3))
+print(modulas(2,3))
+
+
