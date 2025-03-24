dddca23912a72722608f57282c59ef7e11c4e6e5
josephMasu <josephmuhindo089@gmail.com>
2025-03-24 22:41:23 +0300

    fuction

---
+++ 15 Function/one.py
@@ -0,0 +1,9 @@
+def happy_birthday(name):
+    print(f"Happy Birthday to you {name}!")
+    print("How old are you now?")
+    print("Joyeux anniversaire à toi?")
+    print()
+
+happy_birthday("Joe")
+happy_birthday("Masu")
+happy_birthday("Muhindo")
