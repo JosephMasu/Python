1de42518edc7dd630513ab13f05b4f32a9e8a0bb
josephMasu <josephmuhindo089@gmail.com>
2026-09-10 10:27:18 +0300

    ducktyping

--- Polymorphism/one.py
+++ Polymorphism/one.py
@@ -7,6 +7,10 @@ class Laptop1:
 class Desktop1:
     def build(self):
         print("Desktop building")
+# Ducktyping
+class Tablet1:
+    def open_pdf(self):
+        print("Opening PDF on Tablet")
 
 class Alien:
     def code(self, machine: Laptop1):
@@ -14,8 +18,10 @@ class Alien:
         machine.build()
     
 Asur_rog = Laptop1()
-beas=Desktop1()
+beas = Desktop1()
+lenovo = Tablet1()
 
 masu = Alien()
 masu.code(Asur_rog)  
 masu.code(beas) 
+masu.code(lenovo)  # This will raise an error since Tablet1 does not have a build method
